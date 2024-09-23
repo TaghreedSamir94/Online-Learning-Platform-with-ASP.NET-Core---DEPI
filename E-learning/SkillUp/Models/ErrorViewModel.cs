1370a7369d8851c3b71294f526a50ab16530975c
TaghreedSamir94 <taghreedsamir49@gmail.com>
2024-09-23 19:29:56 +0300

    intialize & setup

---
+++ E-learning/SkillUp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SkillUp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
