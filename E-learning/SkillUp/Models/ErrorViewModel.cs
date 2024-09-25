a8cd0c0be6dab88fe4ec43704768e640968e96c8
TaghreedSamir94 <taghreedsamir49@gmail.com>
2024-09-25 19:33:20 +0300

    remove files

--- E-learning/SkillUp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SkillUp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
